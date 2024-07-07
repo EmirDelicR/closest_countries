7ecdaefdebafdf0a703d3dbfeb78e101aaa90d5c
EmirDelicR <emirdelictbf@gmail.com>
2024-07-07 08:23:45 +0200

    feat: setup provider for tanstack react-query library

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import { Paper } from "@mantine/core";
+import IpWidget from "./components/IpWidget";
+import Provider from "./Provider";
 
 export default function HomePage() {
-  return <Paper>Initial setup</Paper>;
+  return (
+    <Provider>
+      <Paper>
+        <IpWidget />
+      </Paper>
+    </Provider>
+  );
 }
